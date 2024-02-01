77999e7205ff321676810eb96365570c1d219f99
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-02-01 14:42:34 +0800

    adjust layer for cyrilic

--- keyboards/splitkb/aurora/sofle_v2/keymaps/eugeny-dementev/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/eugeny-dementev/keymap.c
@@ -7,22 +7,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MNXT,    KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                               KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_VOLD,
         KC_MPLY,    KC_A,   ALT_T(KC_S), CTL_T(KC_D), GUI_T(KC_F),  KC_G,                               KC_H,   GUI_T(KC_J), CTL_T(KC_K), ALT_T(KC_L),  KC_SCLN,    KC_MUTE,
         XXXXXXX,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_MUTE,    XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    MO(3),
-                                KC_GRV,     KC_BSLS,    KC_BSPC, LT(2, KC_TAB), KC_LSFT,    KC_SPC,  LT(1,KC_ENT),  KC_ESC,     KC_LBRC,    KC_RBRC
+                                XXXXXXX,    XXXXXXX,    KC_BSPC, LT(2, KC_TAB), KC_LSFT,    KC_SPC,  LT(1,KC_ENT),  KC_ESC,     XXXXXXX,    XXXXXXX
     ),
 
 	[1] = LAYOUT(
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
-        _______,    KC_LEFT,    KC_PERC,    KC_ASTR,    KC_DQT,     KC_UP,                              KC_DOWN,    KC_AMPR,    KC_QUOT,    KC_QUES,    KC_RGHT,    _______,
+        _______,    KC_GRV,     KC_QUES,    KC_ASTR,    KC_PERC,    KC_DQT,                             KC_QUOT,    KC_AMPR,    KC_LBRC,    KC_RBRC,    KC_EXLM,    _______,
         _______,    KC_CIRC,    KC_EQL,     KC_UNDS,    KC_DLR,     KC_HASH,                            KC_TILD,    KC_COLN,    KC_LPRN,    KC_RPRN,    KC_SCLN,    _______,
-        _______,    KC_LT,      KC_PLUS,    KC_MINS,    KC_GT,      KC_AT,      _______,    _______,    KC_EXLM,    KC_PIPE,    KC_LCBR,    KC_RCBR,    _______,    _______,
-                                _______,    _______,    _______,    KC_TAB,     _______,    _______, LT(1,KC_ENT),  KC_ESC,     _______,    _______
+        _______,    KC_LT,      KC_PLUS,    KC_MINS,    KC_GT,      KC_AT,      _______,    _______,    KC_BSLS,    KC_PIPE,    KC_LCBR,    KC_RCBR,    _______,    _______,
+                                _______,    _______,    _______,    KC_TAB,     _______,    _______,    _______,    KC_ESC,     _______,    _______
     ),
 
 	[2] = LAYOUT(
         _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    _______,    _______,    _______,    KC_PGUP,    _______,                            _______,    KC_PGDN,    _______,    _______,    _______,    _______,
-        _______,    KC_LSFT,    KC_LALT,    KC_LCTL,    KC_HOME,    _______,                            _______,    KC_END,     _______,    _______,    _______,    _______,
-        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    KC_PGUP,    KC_GRV,                             _______,    KC_PGDN,    _______,    _______,    _______,    _______,
+        _______,    KC_LSFT,    KC_LALT,    KC_LCTL,    KC_END,     _______,                            _______,    KC_DOWN,    KC_UP,      KC_LEFT,    KC_RGHT,    _______,
+        _______,    _______,    _______,    _______,    _______,    KC_QUOT,    _______,    _______,    _______,    KC_RBRC,    _______,    _______,    KC_LBRC,    _______,
                                 _______,    _______,    KC_DEL, LT(2, KC_TAB),  _______,    _______,    KC_ENT,     _______,    _______,    _______
     ),
 
